5dc1532c8d9e7c98454cdb8bfa1161545d58fac0
Valentyn Kuznietsov <val@Valentyns-MacBook-Pro.local>
2016-10-02 01:27:42 +0200

    Stub classes

---
+++ src/com/company/parser/primitives/PDFForm.java
@@ -0,0 +1,9 @@
+package com.company.parser.primitives;
+
+/**
+ * Created by val on 02/10/16.
+ */
+public class PDFForm {
+    // represents a form on the pdf file, such as: text field, image, etc.
+    // contains also flags for this element.
+}
---
+++ src/com/company/parser/primitives/PDFPage.java
@@ -0,0 +1,8 @@
+package com.company.parser.primitives;
+
+/**
+ * Created by val on 02/10/16.
+ */
+public class PDFPage {
+    // contains a lot of forms which are on this page and info about this page
+}
