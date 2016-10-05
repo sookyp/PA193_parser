05199915e0cb9dc76af568bed79c17de6ea262f1
Valentyn Kuznietsov <val@Valentyns-MacBook-Pro.local>
2016-10-05 21:17:18 +0200

    Minor changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,13 +2,12 @@ package com.company;
 
 import com.company.parser.Document;
 import com.company.parser.*;
-import com.company.parser.parsers.DocumentParser;
 
 public class Main {
 
     public static void main(String[] args) {
         DocumentLoader loader = new DocumentLoader();
-        Document documentToParse = new DocumentParser(loader.pathForTestPdf()).parseDocument();
+        Document documentToParse = new Document( loader.pathForTestPdf() );
 
         documentToParse.printFileInfo();
     }
