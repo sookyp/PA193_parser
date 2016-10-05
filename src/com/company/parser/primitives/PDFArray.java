48467fb10c3129fb5c472643e7d61d23a70dd868
Valentyn Kuznietsov <val@Valentyns-MacBook-Pro.local>
2016-10-05 21:37:11 +0200

    PDFDictionary, PDFArray implementation (partially)

--- src/com/company/parser/primitives/PDFArray.java
+++ src/com/company/parser/primitives/PDFArray.java
@@ -7,8 +7,17 @@ import java.io.InputStream;
  * Created by val on 02/10/16.
  */
 public class PDFArray implements PDFObject {
+    PDFObject[] objects;
+
     @Override
     public PDFObject objectWithData(InputStream bytes) {
         return null;
+    }
+
+    public PDFObject objectAtIndex(int index){
+        if(index < 0 || index >= objects.length) {
+            return null;
+        }
+        return objects[index];
     }
 }
--- src/com/company/parser/primitives/PDFDictionary.java
+++ src/com/company/parser/primitives/PDFDictionary.java
@@ -2,13 +2,37 @@ package com.company.parser.primitives;
 
 //import sun.jvm.hotspot.runtime.Bytes;
 import java.io.InputStream;
-
+import java.util.*;
 /**
  * Created by val on 02/10/16.
  */
 public class PDFDictionary implements PDFObject {
-    @Override
+    private static final int NOT_FOUND = Integer.MAX_VALUE;
+
+    Map<String, PDFObject> map = new HashMap<String, PDFObject>();
+
+    String[] keys;
+    PDFObject[] values;
+
+
     public PDFObject objectWithData(InputStream bytes) {
         return null;
+    }
+
+    public boolean setObjectForKey(PDFObject object, String key){
+        try{
+            map.put(key, object);
+        } catch (Exception e){
+            return false;
+        }
+        return true;
+    }
+
+    public PDFObject objectForKey(String key){
+        try{
+            return map.get(key);
+        } catch (Exception e){
+            return null;
+        }
     }
 }
